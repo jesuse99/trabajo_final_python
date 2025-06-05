8989e7feedd1387cc271012e3982a7138005a5d2
jesuse99 <jespinoza@uade.edu.ar>
2025-06-04 22:18:33 -0300

    add validate time and list time

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Tareas por realizar (hasta el 11)
 
 [ ] Crear funcion para validar tiempo (hh) (solo numeros)
-[ ] Crear funcion para validar tiempo (mm y ss) (solo numeros)
+[x] Crear funcion para validar tiempo (mm y ss) (solo numeros)
 [ ] Crear funcion para validar numero de participante (solo numeros)
 [ ] Crear funcion para calcular y mostrar promedio
 [ ] Crear lista y guardar los numeros de cada part
-[ ] Crear lista y guardar los tiempos de cada part
+[x] Crear lista y guardar los tiempos de cada part
 [ ] Crear un menu de inicio
 
 # Extras por realizar (hasta el 18)
--- main.py
+++ main.py
@@ -1,30 +1,68 @@
-from datetime import time
-from time import strftime, gmtime
+TIEMPO_RECORD = 1
+tiempo_ganador = 0
+numero_ganador = 0
+tiempo_suma = 0
+tiempo_parts = []
 
-TIME_RECORD = time(4, 42, 15)
-time_win = time(23, 59, 59)
-num_win = 0
-suma = 0
+def validar_tiempo(valor):
+    tiempo_valido = False
+    if (valor < 0 or valor > 59):
+        print("El valor no es valido, vuelva a intentarlo")
+        tiempo_valido = False
+    else:
+        print("Excelente, pasemos a la siguiente")
+        tiempo_valido = True
+    return not tiempo_valido
 
-part = int(input("Ingrese cantidad participantes: "))
-for i in range(part):
+def convertir_tiempo(tiempo_segundos):
+    horas, minutos, segundos = 0,0,0
+    while tiempo_segundos > 0:
+        if tiempo_segundos >= 3600:
+            tiempo_segundos = tiempo_segundos % 3600
+            horas += 1
+        if tiempo_segundos >= 60:
+            tiempo_segundos = tiempo_segundos % 60
+            minutos += 1
+        else:
+            segundos = tiempo_segundos
+            tiempo_segundos = 0
+    return horas, minutos, segundos
+        
+
+parts = int(input("Ingrese cantidad participantes: "))
+for part in range(parts):
     numero = int(input("Ingrese numero de participante: "))
     
-    tiempo = input("Ingrese tiempo de participante (hh/mm/ss): ")
-    tiempo = time(int(tiempo[0:2]), int(tiempo[3:5]), int(tiempo[6:8]))
-    
-    suma += tiempo.second
-    if tiempo < time_win:
-        time_win = tiempo
-        num_win = numero
+    # funcion para validar horas (completar)
+    horas = 1
 
-if part > 0:
-    print("El numero ganador de la carrera:", num_win, "y tiempo que empleo:", time_win)
+    minutos = int(input("Ingrese minutos del participante: "))
+    while validar_tiempo(minutos):
+        minutos = int(input("Ingrese minutos del participante: "))
     
-    time_average = strftime("%H:%M:%S", gmtime(suma/part))
-    print("Tiempo promedio entre ciclistas:", time_average)
+    segundos = int(input("Ingrese segundos del participante: "))
+    while validar_tiempo(segundos):
+        segundos = int(input("Ingrese segundos del participante: "))
 
+    tiempo = (horas * 3600) + (minutos * 60) + segundos 
+    print(tiempo)
+    tiempo_parts.append(tiempo)
+
+    tiempo_suma = tiempo_suma + tiempo
+    if tiempo > tiempo_ganador:
+        tiempo_ganador = tiempo
+        numero_ganador = numero
+
+if parts > 0:
+    horas, minutos, segundos = convertir_tiempo(tiempo_ganador)
+    print("El numero ganador de la carrera:", numero_ganador, "y tiempo que empleo:", f"{horas}:{minutos}:{segundos}")
+    
+    # completar funcion calcular y mostrar promedio
+    tiempo_promedio = 0
+    print("Tiempo promedio entre ciclistas:", tiempo_promedio)
+
+# completar funcion calcular si batio record
 record = input("Ingrese tiempo record (hh/mm/ss): ")
-record = time(int(record[0:2]), int(record[3:5]), int(record[6:8]))
-if record < TIME_RECORD:
+record = 1
+if record < TIEMPO_RECORD:
     print("Nuevo tiempo record! el ganador batio el record")
